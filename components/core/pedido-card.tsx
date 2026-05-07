18c83fe62fecc3be6d1d0e69fab454bea9b6021a
Lúcio santos <lucio.baixada6@gmail.com>
2026-05-07 12:10:46 -0100

    djeison louco

--- components/core/pedido-card.tsx
+++ components/core/pedido-card.tsx
@@ -1,10 +1,11 @@
-import { Card, CardContent } from "../ui/card"
+import { Card, CardContent, CardHeader } from "../ui/card"
+import Image from "next/image"
 
 interface Categoriatype {
     id: number;
     name: string;
     icone: string;
-} 
+}
 
 
 interface PedidoCardProps {
@@ -16,11 +17,31 @@ interface PedidoCardProps {
 
 export const PedidoCard = (PedidoCardProps: PedidoCardProps) => {
     return (
-        <Card>
+
+        <Card className="h-[400px] w-full">
             <CardContent>
-                <div>
-                <h1>Pedido</h1>
+                <div className="w-full h-[200px] relative">
+                    <Image
+                        src={PedidoCardProps.image}
+                        alt={PedidoCardProps.title}
+                        fill
+                        className="object-cover"
+                    />
+
+                    <div className="absolute top-1/2 left-1/2 bg-secondary/80 text-secondary">
+                        <Image
+                            src={PedidoCardProps.category.icone}
+                            alt={PedidoCardProps.category.name}
+                            width={20}
+                            height={20}
+                        />
+
+                    </div>
                 </div>
+                    <div className="p-4">
+                        <h1>Pedido</h1>
+                    </div>
+                    
             </CardContent>
         </Card>
     )
